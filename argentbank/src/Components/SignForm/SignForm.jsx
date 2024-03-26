db4cebeeca42f397fe650266eb44ccef81fa2e63
Lylouf13 <lylian.leutard@gmail.com>
2024-03-26 14:49:56 +0100

    Integrated sign page fully

--- argentbank/src/Components/SignForm/SignForm.jsx
+++ argentbank/src/Components/SignForm/SignForm.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import './signForm.scss'
 
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
 export default function SignForm() {
   return (
-    <div className='sign'>
-      <form className='sign__form'>
-        <label className='sign__form__label' id="username">Username</label>
-        <input className='sign__form__input' type="text"></input>
-        <label className='sign__form__label' id="Password">Password</label>
-        <input className='sign__form__input' type="text"></input>
-        <label className='sign__form__label' id="submit"></label>
-        <button type="button">Sign In</button>
-      </form>
-    </div>
+    <>
+      <Header />
+      <div className='sign'>
+        <form className='sign__form'>
+          <h2>Sign in</h2>
+          <label className='sign__form__label' id="username">Username</label>
+          <input className='sign__form__input' type="text"></input>
+          <label className='sign__form__label' id="Password">Password</label>
+          <input className='sign__form__input' type="text"></input>
+          <label className='sign__form__label' id="submit"></label>
+          <div className='sign__form__remember'>
+            <input type="checkbox" id="remember-me" />
+            <label for="remember-me">Remember me</label>
+          </div>
+          <button className='sign__form__button' type="button">Sign In</button>
+        </form>
+      </div>
+      <Footer />
+    </>
   )
 }
